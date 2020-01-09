32bfdf7d391b98a96100ac65d4bbee0ec49398c7
Claudiu <claudiu.bargianu@gmail.com>
2020-01-09 20:33:04 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 // import {createLogger} from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import * as serviceWorker from './serviceWorker';
-import './index.css';
-import App from './containers/App';
-import {searchRobots, requestRobots} from './reducers';
-import 'tachyons';
+import thunkMiddleware from "redux-thunk";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import App from "./containers/App";
+import { searchRobots, requestRobots } from "./reducers";
 
+import "tachyons";
 
 // const logger = createLogger();
 
-const rootReducer = combineReducers({searchRobots, requestRobots})
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware)); //logger after
 
-
-
-ReactDOM.render(<Provider store={store}>
-                    <App />
-                </Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.register();
-
- 
